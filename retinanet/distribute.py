212c3495a22452d8d28b80c704b1a8807c1d485c
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-07 14:15:25 +0530

    allow tpu name to be overided with ENV VAR `TPU_NAME`

--- retinanet/distribute.py
+++ retinanet/distribute.py
@@ -1,3 +1,5 @@
+import os
+
 import tensorflow as tf
 from absl import logging
 
@@ -17,7 +19,29 @@ def get_strategy(params):
 
     if params.type == 'tpu':
         logging.info('Creating TPU strategy')
+
+        tpu_name = params.name
+
+        if tpu_name == '':
+            if 'TPU_NAME' not in os.environ:
+                raise AssertionError(
+                    'Failed to fetch TPU name, please set ENV VAR \
+                        `TPU_NAME` or specify TPU name in config ')
+
+            tpu_name = os.environ['TPU_NAME']
+            logging.warning(
+                'Using {} as TPU name from ENV VAR `TPU_NAME`'.format(tpu_name))
+
+        else:
+            if 'TPU_NAME' in os.environ:
+                tpu_name = os.environ['TPU_NAME']
+            logging.warning(
+                'Changed TPU name from {} to {} \
+                    (overided with ENV VAR `TPU_NAME`)'.format(
+                    params.name, tpu_name))
+
         resolver = tf.distribute.cluster_resolver.TPUClusterResolver.connect(
-            params.name)
+            tpu_name)
         return tf.distribute.TPUStrategy(resolver)
+
     raise ValueError('Unsupported strategy requested')
