1ce5fdec339fa4f0acb63a9ff66079a565af18b3
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-13 10:53:24 +0530

    implemented InverseDecay LearningRateSchedule

--- retinanet/lr_schedules/__init__.py
+++ retinanet/lr_schedules/__init__.py
@@ -1,4 +1,5 @@
+from retinanet.lr_schedules.inverse_decay import InverseDecay
 from retinanet.lr_schedules.piecewise_constant_decay_with_warmup import \
     PiecewiseConstantDecayWithLinearWarmup
 
-__all__ = ['PiecewiseConstantDecayWithLinearWarmup']
+__all__ = ['InverseDecay', 'PiecewiseConstantDecayWithLinearWarmup']
---
+++ retinanet/lr_schedules/inverse_decay.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+
+class InverseDecay(tf.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, initial_learning_rate, decay_rate):
+        super(InverseDecay, self).__init__()
+
+        self.initial_learning_rate = initial_learning_rate
+        self.decay_rate = decay_rate
+
+    def __call__(self, step):
+        learning_rate = tf.math.divide_no_nan(
+            self.initial_learning_rate,
+            (1.0 + self.decay_rate * tf.cast(step, dtype=tf.float32)))
+        return learning_rate
+
+    def get_config(self):
+        config = {
+            "initial_learning_rate": self.initial_learning_rate,
+            "decay_rate": self.decay_rate,
+        }
+        base_config = super(InverseDecay,
+                            self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
