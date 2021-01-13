271507d9b920eb0d31175db1f491688586ebe188
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-13 11:26:35 +0530

    export PreprocessingPipeline

--- retinanet/dataloader/__init__.py
+++ retinanet/dataloader/__init__.py
@@ -1,4 +1,5 @@
 from retinanet.dataloader.input_pipeline import InputPipeline
+from retinanet.dataloader.preprocessing_pipeline import PreprocessingPipeline
 from retinanet.dataloader.utils import normalize_image
 
-__all__ = ['InputPipeline', 'normalize_image']
+__all__ = ['InputPipeline', 'normalize_image', 'PreprocessingPipeline']
