237508cccd0c257565993a5b146bac0c137e48d5
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-24 01:37:57 +0530

    fix import errors

--- retinanet/dataloader/__init__.py
+++ retinanet/dataloader/__init__.py
@@ -1,7 +1,5 @@
 from retinanet.dataloader.input_pipeline import InputPipeline
-from retinanet.dataloader.preprocessing_pipeline import PreprocessingPipeline
-from retinanet.dataloader.preprocessing_pipeline_v2 import PreprocessingPipelineV2
 from retinanet.dataloader.utils import normalize_image
 
-__all__ = ['InputPipeline', 'normalize_image',
-           'PreprocessingPipeline', 'PreprocessingPipelineV2']
+
+__all__ = ['InputPipeline', 'normalize_image', 'PreprocessingPipeline']
