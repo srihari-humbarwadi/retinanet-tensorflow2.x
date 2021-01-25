3b0a2f6c208e6bfbdf3686d2a09d3d071ac1e63f
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-25 07:46:30 +0530

    log average number of anchors matched per image

--- retinanet/executor.py
+++ retinanet/executor.py
@@ -265,7 +265,7 @@ class Executor:
         self.optimizer.apply_gradients(
             zip(gradients, self._model.trainable_variables))
 
-        loss['num-anchors-matched'] *= self.num_replicas
+        loss['num-anchors-matched'] /= tf.shape(images)[0]
         loss['gradient-norm'] = tf.linalg.global_norm(gradients) * self.num_replicas
         return loss
 
