ac918159e730ce437b625c9872fea588dd206eb7
Kartik Sharma <kartik4949@gmail.com>
2021-01-26 23:26:21 +0530

    :hammer: typing hinting fix

--- retinanet/cfg/config.py
+++ retinanet/cfg/config.py
@@ -1,6 +1,6 @@
 """ Hyperparameters for trainer and model. """
 import json
-from typing import Text
+from typing import Text, List, Dict
 
 from absl import logging
 from easydict import EasyDict
@@ -19,19 +19,19 @@ class Config:
             self._params = EasyDict(json.load(fp))
 
     @property
-    def params(self):
+    def params(self) -> EasyDict:
         return self._params
 
     @property
-    def keys(self):
+    def keys(self) -> List:
         return self.config_dict.keys()
 
     @classmethod
-    def _parser(cls, config: Text):
+    def _parser(cls, config: Text) -> Dict:
         config_dict = json.loads(config)
         return config_dict
 
-    def override(self, config: Text):
+    def override(self, config: Text) -> EasyDict:
         assert isinstance(config, str), "wrong type for hparams"
         try:
             config_dict = self._parser(config)
