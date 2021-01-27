95aa2a5f259508096474c5e0d9d966bbfbda2cd6
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-28 05:07:24 +0530

    subclassed `FPN` with a more robust impl

--- retinanet/model/neck/fpn.py
+++ retinanet/model/neck/fpn.py
@@ -9,76 +9,89 @@ from retinanet.model.builder import NECK
 
 @NECK.register_module('fpn_v1')
 class FPN(tf.keras.Model):
-    """ FPN builder class """
 
-    def __init__(self, feature_shapes, filters):
+    def __init__(self,
+                 filters=256,
+                 min_level=3,
+                 max_level=7,
+                 backbone_max_level=5,
+                 **kwargs):
+        super(FPN, self).__init__(**kwargs)
 
-        if not isinstance(feature_shapes, (list, tuple)):
-            raise AssertionError(
-                'Expected `inputs` to be either list or tuple, got {} of type: {}'
-                .format(feature_shapes, type(feature_shapes)))
-
-        self.feature_shapes = feature_shapes
         self.filters = filters
+        self.min_level = min_level
+        self.max_level = max_level
+        self.backbone_max_level = backbone_max_level
 
         conv_2d_op = tf.keras.layers.Conv2D
         normalization_op = get_normalization_op()
-        bn_op = functools.partial(
-            normalization_op,
-            momentum=0.997,
-            epsilon=1e-4)
 
-        upsample_op = functools.partial(NearestUpsampling2D, scale=2)
-        relu_op = functools.partial(tf.keras.layers.ReLU)
+        self.upsample_op = functools.partial(NearestUpsampling2D, scale=2)
+        self.lateral_convs = {}
+        self.output_convs = {}
+        self.output_norms = {}
+        self.relu_ops = {}
 
-        inputs = [tf.keras.Input(shape=shape[1:]) for shape in feature_shapes]
-        c3, c4, c5 = inputs
+        for level in range(min_level, backbone_max_level + 1):
+            level = str(level)
+            self.lateral_convs[level] = conv_2d_op(
+                filters=self.filters,
+                kernel_size=1,
+                strides=1,
+                kernel_initializer=tf.initializers.VarianceScaling(),
+                padding='same',
+                name='l' + str(level))
 
-        min_level = 3
-        max_level = 7
+        for level in range(min_level, max_level + 1):
+            level = str(level)
+            self.output_norms[level] = normalization_op(
+                name='p' + str(level) + '-bn')
 
-        lateral_convs = []
-        output_convs = []
-        output_bns = []
+            self.output_convs[level] = conv_2d_op(
+                filters=self.filters,
+                kernel_size=3,
+                kernel_initializer=tf.initializers.VarianceScaling(),
+                padding='same',
+                strides=1 if int(level) < min_level + 3 else 2,
+                name='p' + str(level))
 
-        conv2d_same_pad = functools.partial(
-            conv_2d_op,
-            filters=self.filters,
-            kernel_initializer=tf.initializers.VarianceScaling(),
-            padding='same')
+        for level in range(backbone_max_level + 1, max_level):
+            level = str(level)
+            name = 'p{}-relu'.format(level)
+            self.relu_ops[level] = tf.keras.layers.ReLU(name=name)
 
-        for i in range(min_level, max_level + 1):
-            if i < min_level + 3:
-                lateral_convs += [
-                    conv2d_same_pad(kernel_size=1, strides=1, name='l' + str(i))
-                ]
-            strides = 1 if i < min_level + 3 else 2
+    def call(self, features, training=None):
+        outputs = {}
 
-            # TODO consider removing redundant bias from BN(Conv2d + bias)
-            output_convs += [
-                conv2d_same_pad(kernel_size=3, strides=strides, name='p' + str(i))
-            ]
+        for level in range(self.min_level, self.backbone_max_level + 1):
+            level = str(level)
+            conv_layer = self.lateral_convs[level]
+            outputs[level] = conv_layer(features[level])
 
-            output_bns += [bn_op(name='p' + str(i) + '-bn')]
+        for level in range(self.backbone_max_level, self.min_level, -1):
+            level = str(level)
+            name = 'm{}-upsample'.format(int(level) - 1)
+            outputs[str(int(level) - 1)] += self.upsample_op(name=name)(
+                outputs[level])
 
-        l3, l4, l5 = [conv(x) for conv, x in zip(lateral_convs, (c3, c4, c5))]
+        for level in range(self.min_level, self.max_level + 1):
+            level = str(level)
+            if int(level) <= self.backbone_max_level:
+                outputs[level] = self.output_convs[level](outputs[level])
 
-        m4 = tf.keras.layers.Add(name='m4')([l4, upsample_op(name='m4_up')(l5)])
-        m3 = tf.keras.layers.Add(name='m3')([l3, upsample_op(name='m3_up')(m4)])
+            elif int(level) == self.backbone_max_level + 1:
+                outputs[level] = self.output_convs[level](
+                    outputs[str(int(level) - 1)])
 
-        p3 = output_convs[0](m3)
-        p4 = output_convs[1](m4)
-        p5 = output_convs[2](l5)
-        p6 = output_convs[3](p5)
-        p7 = output_convs[4](relu_op(name='p6-relu')(p6))
+            else:
+                prev_level_output = \
+                    self.relu_ops[str(int(level) - 1)](outputs[str(int(level) - 1)])
 
-        outputs = [bn(x) for bn, x in zip(output_bns, (p3, p4, p5, p6, p7))]
-        super(FPN, self).__init__(inputs=inputs, outputs=outputs, name='fpn')
+                outputs[level] = self.output_convs[level](prev_level_output)
 
-    def get_config(self):
-        config = {
-            "feature_shapes": self.feature_shapes,
-            "filters": self.filters,
-        }
-        base_config = super(FPN, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+        for level in range(self.min_level, self.max_level + 1):
+            level = str(level)
+            outputs[level] = self.output_norms[level](
+                outputs[level], training=training)
+
+        return outputs
