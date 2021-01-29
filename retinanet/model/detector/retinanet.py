4125aa22f40b09008d67e116152796f7ec439b95
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-29 23:32:00 +0530

    changed `fpn` to `neck`

--- retinanet/model/detector/retinanet.py
+++ retinanet/model/detector/retinanet.py
@@ -7,13 +7,13 @@ from retinanet.model.builder import DETECTOR
 class RetinaNet(tf.keras.Model):
     """ RetinaNet detector class. """
 
-    def __init__(self, backbone, fpn, box_head, class_head, **kwargs):
+    def __init__(self, backbone, neck, box_head, class_head, **kwargs):
         image_inputs = tf.keras.Input(shape=backbone.input.shape[1:], name="images")
 
         features = backbone(image_inputs)
-        fpn_features = fpn(features)
-        box_outputs = box_head(fpn_features)
-        class_outputs = class_head(fpn_features)
+        neck_features = neck(features)
+        box_outputs = box_head(neck_features)
+        class_outputs = class_head(neck_features)
 
         outputs = {
             'class-predictions': class_outputs,
