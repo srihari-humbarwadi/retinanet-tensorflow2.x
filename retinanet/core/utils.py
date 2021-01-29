`get_optimizer` now handles mixed precision training; fix params being overitten

retinanet/core/utils.py
@@ -1,23 +1,29 @@
 import os
+from copy import deepcopy
 
 import tensorflow as tf
 from absl import logging
-
 from retinanet.core.optimizer import PiecewiseConstantDecayWithLinearWarmup
 
 
-def get_optimizer(params):
-    lr_params = params.pop('lr_params', None)
+def get_optimizer(params, precision):
+    _params = deepcopy(params)
+    lr_params = _params.pop('lr_params', None)
     learning_rate_fn = PiecewiseConstantDecayWithLinearWarmup(
         lr_params.warmup_learning_rate, lr_params.warmup_steps,
         lr_params.boundaries, lr_params.values)
-    params['learning_rate'] = learning_rate_fn
+    _params['learning_rate'] = learning_rate_fn
 
     config = {
-        'class_name': params['name'],
-        'config': params
+        'class_name': _params['name'],
+        'config': _params
     }
+
     optimizer = tf.optimizers.get(config)
+    if precision == 'mixed_float16':
+        logging.info('Wrapping optimizer with `LossScaleOptimizer` for AMP training')
+        optimizer = tf.keras.mixed_precision.LossScaleOptimizer(
+            optimizer, dynamic=True)
 
     return optimizer
 