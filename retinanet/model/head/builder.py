4c50f4c7ed0b06086dc9e68495d8a4b02fcf635d
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-02-04 03:50:20 +0530

    added builders

---
+++ retinanet/model/head/builder.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+
+from retinanet.model.head.detection_head import DetectionHead
+
+
+def build_heads(params, min_level, max_level):
+    box_head = DetectionHead(
+        num_convs=params.num_convs,
+        filters=params.filters,
+        output_filters=params.num_anchors * 4,
+        min_level=min_level,
+        max_level=max_level,
+        prediction_bias_initializer='zeros',
+        name='box-head')
+
+    prior_prob_init = tf.constant_initializer(-np.log((1 - 0.01) / 0.01))
+    class_head = DetectionHead(
+        num_convs=params.num_convs,
+        filters=params.filters,
+        output_filters=params.num_anchors*params.num_classes,
+        min_level=min_level,
+        max_level=max_level,
+        prediction_bias_initializer=prior_prob_init,
+        name='class-head')
+
+    return box_head, class_head
---
+++ retinanet/optimizers/builder.py
@@ -0,0 +1,35 @@
+from copy import deepcopy
+import json
+
+import tensorflow as tf
+from absl import logging
+
+from retinanet.optimizers.piecewise_constant_decay_with_warmup import \
+    PiecewiseConstantDecayWithLinearWarmup
+
+
+def build_optimizer(params, precision):
+    _params = deepcopy(params)
+    lr_params = _params.pop('lr_params', None)
+    learning_rate_fn = PiecewiseConstantDecayWithLinearWarmup(
+        lr_params.warmup_learning_rate, lr_params.warmup_steps,
+        lr_params.boundaries, lr_params.values)
+    _params['learning_rate'] = learning_rate_fn
+
+    config = {
+        'class_name': _params['name'],
+        'config': _params
+    }
+
+    optimizer = tf.optimizers.get(config)
+    if precision == 'mixed_float16':
+        logging.info(
+            'Wrapping optimizer with `LossScaleOptimizer` for AMP training')
+        optimizer = tf.keras.mixed_precision.LossScaleOptimizer(
+            optimizer, dynamic=True)
+
+    logging.info(
+        'Optimizer Config: \n{}'
+        .format(json.dumps(optimizer.get_config(), indent=4)))
+
+    return optimizer
