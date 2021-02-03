3ed40d0b1255005c2963343115e607c4a53d18c3
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-02-04 03:47:44 +0530

    reorganizing layers

---
+++ retinanet/model/layers/__init__.py
@@ -0,0 +1,4 @@
+from retinanet.model.layers.decode_predictions import DecodePredictions
+from retinanet.model.layers.nearest_upsampling import NearestUpsampling2D
+
+__all__ = ['DecodePredictions', 'NearestUpsampling2D']
