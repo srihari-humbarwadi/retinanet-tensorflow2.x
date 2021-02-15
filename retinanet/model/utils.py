5710ff7f963363f3bb1e4285ce698793b7277569
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-02-16 00:11:51 +0530

    removed `TPUBatchNormalization`

--- retinanet/model/utils.py
+++ retinanet/model/utils.py
@@ -3,24 +3,13 @@ import functools
 import tensorflow as tf
 from absl import logging
 
-from retinanet.model.layers.tpu_batch_normalization import TpuBatchNormalization
-
 
 def get_normalization_op(**params):
-    strategy = tf.distribute.get_strategy()
-    num_replicas = strategy.num_replicas_in_sync
+    num_replicas = tf.distribute.get_strategy().num_replicas_in_sync
 
-    if params.get('use_sync', None):
-        if isinstance(strategy, tf.distribute.TPUStrategy):
-            logging.debug('Using `{}`'.format('TpuBatchNormalization'))
-            op = TpuBatchNormalization
-
-        elif num_replicas > 1:
-            logging.debug('Using `{}`'.format('SyncBatchNormalization'))
-            op = tf.keras.layers.experimental.SyncBatchNormalization
-
-        else:
-            op = tf.keras.layers.BatchNormalization
+    if params.get('use_sync', None) and num_replicas > 1:
+        logging.debug('Using `{}`'.format('SyncBatchNormalization'))
+        op = tf.keras.layers.experimental.SyncBatchNormalization
 
     else:
         op = tf.keras.layers.BatchNormalization
