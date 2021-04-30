e624d9a9bd52d7e684df9c9ea8e8848e4d1d10b5
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-04-30 13:16:32 +0530

    temporary launch script

--- launch.sh
+++ launch.sh
@@ -1,5 +1,3 @@
-for file in configs/*
-do
-echo launching $file ;
-screen -dmS $(basename $file .json) python3 -m retinanet.main --config_path $file --xla --debug ;
-done
+export TPU_NAME=$2
+echo launching $1
+screen -dmS $(basename $1 .json) python3 -m retinanet --is_multi_host --log_dir logs --alsologtostderr --config_path $1
