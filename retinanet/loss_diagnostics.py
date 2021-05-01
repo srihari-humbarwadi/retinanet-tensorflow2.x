e1275228e5180437d067c8c54334fe3d2bde9a7e
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-01 19:55:49 +0530

    added InflectionDetector to detect loss blowup

---
+++ retinanet/loss_diagnostics.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+class InflectionDetector:
+    def __init__(self, name, threshold):
+        self.name = name
+        self.threshold = threshold
+
+        self._data = []
+        self._grads = None
+        self._diffs = None
+
+    def is_value_anamolous(self, value):
+        result = False
+        self._data += [value]
+
+        if len(self._data) > 1:
+            self._grads = np.gradient(np.gradient(self._data))
+            self._diffs = np.round(np.abs(np.diff(self._grads)), 3)
+            result = self._diffs[-1] > self.threshold
+
+        return result
+
+    @property
+    def data(self):
+        return self._data
