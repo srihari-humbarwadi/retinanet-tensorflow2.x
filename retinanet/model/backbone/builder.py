2432e07ad68133b6af210b2fb1b0ae7fa6e772a6
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-03 00:18:20 +0530

    allow more granular config parameters for backbones

--- retinanet/model/backbone/builder.py
+++ retinanet/model/backbone/builder.py
@@ -1,13 +1,16 @@
+from copy import deepcopy
 from retinanet.model.backbone.resnet import ResNet
 
 
 def build_backbone(input_shape, params, normalization_op_params=None):
     if params.type == 'resnet':
+        resnet_params = deepcopy(params)
+        resnet_params.pop('type')
+
         model = ResNet(
             input_shape=input_shape,
-            depth=params.depth,
-            checkpoint=params.checkpoint,
-            normalization_op_params=normalization_op_params)
+            normalization_op_params=normalization_op_params,
+            **resnet_params)
 
     else:
         raise ValueError('{} backbone not implemented'.format(
