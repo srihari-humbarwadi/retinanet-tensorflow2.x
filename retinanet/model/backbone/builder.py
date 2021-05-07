deaa2f34801039ab2f18bc9a105e819b9e3ab0bc
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-07 13:07:12 +0530

    added efficientnet in backbone builder

--- retinanet/model/backbone/builder.py
+++ retinanet/model/backbone/builder.py
@@ -1,18 +1,25 @@
 from copy import deepcopy
 from retinanet.model.backbone.resnet import ResNet
+from retinanet.model.backbone.efficientnet import EfficientNet
 
 
 def build_backbone(input_shape, params, normalization_op_params=None):
-    if params.type == 'resnet':
+    if 'resnet' in params.type.lower():
         resnet_params = deepcopy(params)
         resnet_params.pop('type')
 
-        model = ResNet(
+        return ResNet(
             input_shape=input_shape,
             normalization_op_params=normalization_op_params,
             **resnet_params)
 
-    else:
-        raise ValueError('{} backbone not implemented'.format(
-            params.type))
-    return model
+    if 'efficientnet' in params.type.lower():
+        return EfficientNet(
+            input_shape=input_shape,
+            model_name=params.type,
+            checkpoint=params.checkpoint,
+            normalization_op_params=normalization_op_params,
+            override_params=params.get('override_params', None))
+
+    raise ValueError('{} backbone not implemented'.format(
+        params.type))
