3b40403d60b19dd323c65ff250896f65cad81d43
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-07 18:31:59 +0530

    implemented `AverageMeter`

---
+++ retinanet/utils.py
@@ -0,0 +1,28 @@
+class AverageMeter:
+
+    def __init__(self, name=None, momentum=0.997):
+        if momentum >= 1 or momentum <= 0:
+            raise AssertionError('`momentum` should be a non zero float less than 1')
+
+        self.name = name
+        self.momentum = momentum
+        self._averaged_value = None
+        self._count = 0
+
+    def accumulate(self, x):
+        if self._count < 10:
+            self._averaged_value = x
+
+        else:
+            self._averaged_value = \
+                self._averaged_value * self.momentum + (1 - self.momentum) * x
+
+        self._count += 1
+
+    @property
+    def averaged_value(self):
+        return self._averaged_value
+
+    @property
+    def data(self):
+        return self._data
