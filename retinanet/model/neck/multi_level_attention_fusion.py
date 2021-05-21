e7be29bda4b8a6379c1088a50e4565a9f577b325
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-21 15:35:22 +0530

    fixed bugs

--- retinanet/model/neck/multi_level_attention_fusion.py
+++ retinanet/model/neck/multi_level_attention_fusion.py
@@ -58,6 +58,7 @@ class MultiLevelAttentionFusion(tf.keras.layers.Layer):
                                                        kernel_size=1,
                                                        strides=1,
                                                        padding='same',
+                                                       use_bias=conv_2d_op_params.use_bias_before_bn,
                                                        name='l' + str(level) +
                                                        '-conv2d',
                                                        **kernel_initializer_config)
@@ -65,6 +66,7 @@ class MultiLevelAttentionFusion(tf.keras.layers.Layer):
             self.projection_convs[level] = conv_2d_op(
                 filters=projection_dim,
                 kernel_size=1,
+                use_bias=conv_2d_op_params.use_bias_before_bn,
                 name='l' + str(level) + '-projection-conv2d')
 
             self.attention_convs[level] = conv_2d_op(
