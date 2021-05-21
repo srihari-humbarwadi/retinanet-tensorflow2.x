4b96bc1006ca3b5ddcc86fca5cc7b0d142e3198f
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-21 12:49:42 +0530

    added `skip_steps` arg for `InflectionDetector`

--- retinanet/loss_diagnostics.py
+++ retinanet/loss_diagnostics.py
@@ -2,10 +2,14 @@ import numpy as np
 
 
 class InflectionDetector:
-    def __init__(self, name, threshold):
+    def __init__(self, name, threshold, skip_steps=45):
+        if skip_steps < 2:
+            raise ValueError('`skip_steps` should be greater than 2')
+
         self.name = name
         self.threshold = threshold
 
+        self._skip_steps = skip_steps
         self._data = []
         self._grads = None
         self._diffs = None
@@ -14,7 +18,7 @@ class InflectionDetector:
         result = False
         self._data += [value]
 
-        if len(self._data) > 2:
+        if len(self._data) > self._skip_steps:
             self._grads = np.gradient(np.gradient(self._data))
             self._diffs = np.round(np.abs(np.diff(self._grads)), 3)
             result = self._diffs[-2] > self.threshold
