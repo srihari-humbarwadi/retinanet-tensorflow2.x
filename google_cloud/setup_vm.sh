58e13b049643a8284a311768bc80a8de55497e33
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-05-21 11:28:58 +0530

    resnet50 config

--- google_cloud/setup_vm.sh
+++ google_cloud/setup_vm.sh
@@ -2,7 +2,11 @@ sudo fallocate -l 15G /swapfile;
 sudo chmod 600 /swapfile;
 sudo mkswap /swapfile;
 sudo swapon /swapfile
-sudo apt install htop tree -y
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh htop tree -y
 
 wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip
 unzip annotations_trainval2017.zip
